4daacc447324976061d3f90f303b0254fe073caa
ectolitol <tolitolerikaa@gmail.com>
2024-10-24 22:36:02 +0800

    '

--- frontend/src/config/axiosConfig.jsx
+++ frontend/src/config/axiosConfig.jsx
@@ -3,15 +3,15 @@ import axios from "axios";
 // Set the base URL dynamically based on the environment
 axios.defaults.baseURL =
   window.location.hostname === "localhost"
-    ? "http://localhost:4000/" // Development (local)
-    : "http://localhost:4000/"; // Production (use the same base URL)
+    ? "https://eelms-api.onrender.com/" // Development (local)
+    : "https://eelms-api.onrender.com/"; // Production (use the same base URL)
 
 axios.defaults.withCredentials = true; // Ensure credentials (cookies, auth headers) are sent with each request if necessary
 
 export const imageBaseURL =
   window.location.hostname === "localhost"
-    ? "http://localhost:4000" // Development (local)
-    : "http://localhost:4000"; // Production (same image base URL)
+    ? "https://eelms-api.onrender.com" // Development (local)
+    : "https://eelms-api.onrender.com"; // Production (same image base URL)
 
 export default axios;
 
